f3367527e7c6de4934bdbb08e9b385c7f1e15811
Zoff <zoff99@users.noreply.github.com>
2016-05-31 15:54:43 +0200

    gen

--- src/main/java/com/zoffcc/tools/PseudoBitcoinAddress.java
+++ src/main/java/com/zoffcc/tools/PseudoBitcoinAddress.java
@@ -4,4 +4,9 @@ import org.bitcoinj.core.Utils2;
 
 public class PseudoBitcoinAddress
 {
+  
+  static String generate_address()
+  {
+    return org.bitcoinj.core.Utils2.gen_pseudo_BTC_address();
+  }
 }
